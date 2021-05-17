ec5272135942c126481476e750444a7117220977
FrederikWvB <frederik.xiao@gmail.com>
2021-05-17 14:31:36 +0200

    Læs fra fil funktionalitet

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.io.FileNotFoundException;
+
 public class Main
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+
+
+        System.out.println(Member.createMemberList().get(0).name);
 
     }
 }
--- src/Member.java
+++ src/Member.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Member {
     String name;
     int age;
@@ -5,6 +10,39 @@ public class Member {
     public Member(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+
+    public static ArrayList<Member> createMemberList() throws FileNotFoundException {
+        File memberfile = new File("resources/medlemsliste.csv");
+        Scanner fileScanner = new Scanner(memberfile);
+
+        ArrayList<Member> memberArrayList = new ArrayList<>();
+
+        fileScanner.nextLine();
+
+
+        while (fileScanner.hasNext()){
+            String currentLine = fileScanner.nextLine();
+            String[] lineAsArray = currentLine.split(";");
+
+            String name = lineAsArray[0];
+            int age = tryParse(lineAsArray[1]);
+
+            Member tempMember = new Member(name,age);
+
+            memberArrayList.add(tempMember);
+        }
+
+        return memberArrayList;
+    }
+
+    public static int tryParse(String text) {
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
     }
 
 }
